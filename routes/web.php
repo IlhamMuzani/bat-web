7d183a0d74273be0a43d5e8e356580a921d570e1
IlhamMuzani <ilhammuzani21@gmail.com>
2024-08-15 08:19:53 +0700

    data marketing

--- routes/web.php
+++ routes/web.php
@@ -751,6 +751,7 @@ Route::middleware('admin')->prefix('admin')->group(function () {
     Route::delete('inquery_klaimperalatan/deletedetailklaim/{id}', [\App\Http\Controllers\Admin\InqueryKlaimperalatanController::class, 'deletedetailklaim']);
     Route::get('klaim_peralatan/cetak-pdf/{id}', [\App\Http\Controllers\Admin\KlaimperalatanController::class, 'cetakpdf']);
     Route::get('hapusperalatan/{id}', [\App\Http\Controllers\Admin\InqueryKlaimperalatanController::class, 'hapusperalatan'])->name('hapusperalatan');
+    Route::resource('marketing', \App\Http\Controllers\Admin\MarketingController::class);
 
       Route::resource('alamat_muat', \App\Http\Controllers\Admin\AlamatmuatController::class);
     Route::resource('alamat_bongkar', \App\Http\Controllers\Admin\AlamatbongkarController::class);
