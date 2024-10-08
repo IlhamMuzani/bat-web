0b1b290aefeffb8b11328402b2056651a9caf67a
IlhamMuzani <ilhammuzani21@gmail.com>
2024-10-08 08:48:13 +0700

    pengecekan tromol

--- app/Models/Penggantian_bearing.php
+++ app/Models/Penggantian_bearing.php
@@ -18,7 +18,21 @@ class Penggantian_bearing extends Model
         'user_id',
         'kode_penggantian',
         'kendaraan_id',
+        'km_penggantian',
+        'km_berikutnya',
         'tanggal_penggantian',
+        'tromol1',
+        'tromol2',
+        'tromol3',
+        'tromol4',
+        'tromol5',
+        'tromol6',
+        'tromol7',
+        'tromol8',
+        'tromol9',
+        'tromol10',
+        'tromol11',
+        'tromol12',
         'tanggal_awal',
         'tanggal_akhir',
         'status',
@@ -47,7 +61,7 @@ class Penggantian_bearing extends Model
 
     public function detail_penggantianbearing()
     {
-        return $this->hasMany(detail_penggantianbearing::class);
+        return $this->hasMany(Detail_penggantianbearing::class);
     }
 
     public static function getId()
