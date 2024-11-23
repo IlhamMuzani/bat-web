89dd09a84928f00d22714bb9e9da2d6d36ce4792
IlhamMuzani <ilhammuzani21@gmail.com>
2024-11-23 09:48:33 +0700

    perbaiakn spk

--- resources/views/admin/spk/index.blade.php
+++ resources/views/admin/spk/index.blade.php
@@ -234,11 +234,16 @@
                                                     Digunakan Oleh Memo
                                                     Ekspedisi
                                                     <strong>
-                                                        <a href="{{ route('memo_ekspedisi.show', $buktipotongpajak->memo_ekspedisi->first()->id) }}"
-                                                            target="_blank" rel="noopener noreferrer">
-                                                            {{ $buktipotongpajak->memo_ekspedisi->first()->kode_memo }}
-                                                        </a>
+                                                        @if ($buktipotongpajak->memo_ekspedisi && $buktipotongpajak->memo_ekspedisi->isNotEmpty())
+                                                            <a href="{{ route('memo_ekspedisi.show', $buktipotongpajak->memo_ekspedisi->first()->id) }}"
+                                                                target="_blank" rel="noopener noreferrer">
+                                                                {{ $buktipotongpajak->memo_ekspedisi->first()->kode_memo }}
+                                                            </a>
+                                                        @else
+                                                            <span>Memo tidak tersedia</span>
+                                                        @endif
                                                     </strong>
+
                                                 </p>
                                             </div>
                                         </td>
